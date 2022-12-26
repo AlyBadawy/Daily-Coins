a6be6acc12adfae18afe3aea5d218b3c560d453f
Aly Badawy <AlyBadawy@users.noreply.github.com>
2022-12-26 17:27:21 -0500

    Initial Commit

---
+++ Daily Coins/Daily_CoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_CoinsApp.swift
+//  Daily Coins
+//
+//  Created by Aly Badawy on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Daily_CoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
